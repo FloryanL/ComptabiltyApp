3979f09d5e3494d97a130d1d99f0e77f7f31d10a
FloryanL <floryan.lollivier@gmail.com>
2016-10-17 11:57:27 +0200

    first commit & add folders

---
+++ js/controllers.js
@@ -0,0 +1,32 @@
+var angularComptApp = angular.module('ComptApp', ['ngRoute']);
+
+angularComptApp.config(function($routeProvider){
+    $routeProvider
+        .when('/home', {templateUrl: 'parties/home.html'})
+        .when('/compte', {templateUrl: 'parties/compte.html'})
+        .when('/contact', {templateUrl: 'parties/contact.html'})
+        .otherwise({redirecTo : '/'});
+});
+
+
+angularComptaApp.controller('postCtrl', function ($scope) {
+    $scope.credits = [{
+        'name': 'Créditeur 1',
+        'info': 'Description & date'
+    }, {
+        'name': 'Créditeur 2',
+        'info': 'Description & date'
+    }, {
+        'name': 'Créditeur 3',
+        'info': 'Description & date'
+    }, {
+        'name': 'Créditeur 4',
+        'info': 'Description & date'
+    }, {
+        'name': 'Créditeur 5',
+        'info': 'Description & date'
+    }, {
+        'name': 'Créditeur 6',
+        'info': 'Description & date'
+    }]
+});
