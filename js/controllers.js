23bcbb92541ce1aac4966fe2bda5cb4cc06f8cdf
FloryanL <floryan.lollivier@gmail.com>
2016-10-24 12:15:20 +0200

    delete file inscription

--- js/controllers.js
+++ js/controllers.js
@@ -1,28 +1,50 @@
 var angularComptApp = angular.module('ComptApp', ['ngRoute']);
 
-angularComptApp.config(function($routeProvider){
-    $routeProvider
-        .when('/home', {templateUrl: 'parties/home.html'})
-        .when('/compte', {templateUrl: 'parties/compte.html'})
-        .when('/deco', {templateUrl: 'parties/deco.html'})
-        .otherwise({redirecTo : '/'});
+angularComptApp.config(function ($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'parties/home.html'
+    })
+    .when('/compte', {
+      templateUrl: 'parties/compte.html'
+    })
+    .when('/deco', {
+      templateUrl: 'parties/deco.html'
+    })
+    .otherwise({
+      redirecTo: '/'
+    });
 });
 
 
-angularComptApp.controller('logCtrl', function ($scope,$http){
-  $http({
-     method:"GET",
-     url:"json/users.json"
-  }).then(function(response){
-     $scope.users = response.data.records;
+  angularComptApp.controller('logCtrl', function ($scope, $http) {
+    $http({
+      method: "GET",
+      url: "json/users.json"
+    }).then(function (response) {
+      $scope.users = response.data.records;
+    });
   });
-});
-angularComptApp.controller('postCtrl', function ($scope,$http){
-  $http({
-     method:"GET",
-     url:"json/depenses.json"
-  }).then(function(response){
-     $scope.depenses = response.data.records;
+  angularComptApp.controller('postCtrl', function ($scope, $http) {
+    $http({
+      method: "GET",
+      url: "json/depenses.json"
+    }).then(function (response) {
+      $scope.depenses = response.data.records;
+    });
   });
-});
 
+  angularComptApp.controller('registerCtrl', function ($scope) {
+  $scope.showAlert = function (ev) {
+
+    $mdDialog.show(
+      $mdDialog.alert()
+      .parent(angular.element(document.querySelector('#popupContainer')))
+      .clickOutsideToClose(true)
+      .title('Ici le formulaire d\'inscription en popup.')
+      .textContent('Vous êtes bien inscrit.')
+      .ariaLabel('Alert Dialog Demo')
+      .ok('Got it!')
+      .targetEvent(ev)
+    )};
+  });
